dc4ec1af496dd6198c440388985fd40a21d2977a
Phodi-P <5670253+Phodi-P@users.noreply.github.com>
2020-03-03 15:49:02 +0700

    1

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+    getStyle =  () => {
+        return  {
+          background: '#f4f4f4',
+          padding: '10px',
+          borderBottom: '1px #ccc dotted',
+        //   textDecoration: this.props.todo.completed ? 'line-through':'none'
+        }
+    }
+    render() {
+        return (
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" />
+                    todo info
+                    <button style={btnStyle}></button>
+                </p>
+            </div>
+        );
+    }
+}
+
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 8px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+  }
+
+export default TodoItem;
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Item from "./TodoItem"
 
 class Todos extends Component {
     render() {
         return (
             <div>
                 <h4>Todos Components</h4>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
             </div>
         );
     }
