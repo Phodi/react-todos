ab4b7aa3c79517e43f940d22ee15875d3cc1b6e9
Phodi-P <5670253+Phodi-P@users.noreply.github.com>
2020-03-03 16:32:47 +0700

    add key

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -5,6 +5,7 @@ class Todos extends Component {
     render() {
         return this.props.todos.map( todo => (
         <Item todo_item={todo}
+                key={todo.id}
                 markComplete={this.props.markComplete}
                 delTodo={this.props.delTodo} />
         ))
