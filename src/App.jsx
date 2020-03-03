c466dc9e7acf1c10c9b9d01659e505303a693609
Phodi-P <5670253+Phodi-P@users.noreply.github.com>
2020-03-03 15:38:19 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Todos from './components/Todos'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h3>AAA</h3>
+        <Todos/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todos.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Todos Components</h4>
+            </div>
+        );
+    }
+}
+
+export default Todos;
